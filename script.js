cf9a8eddd834d30c3ab0fed7a13e78dca14955e2
Mihir-72 <144117994+Mihir-72@users.noreply.github.com>
2023-10-31 17:49:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// document.getElementsByClassName
+
+// document.getElementById("mihir").innerHTML = odnv;
+// document.getElementById("mihir").background = red;
+
+// function hahaha(){
+//   document.getElementById("mihir").innerHTML = "haha";
+// }
+
+// hahaha();
+
+
+
+function blue(){
+  document.getElementById('karna').style.backgroundColor = "#0dcaf0";
+  document.body.style.backgroundColor = "blue";
+
+  document.getElementById("hi").style.color = "white";
+
+  document.getElementById("c").style.color = "white"
+
+}
+
+function grey(){
+  document.body.style.backgroundColor = "grey";
+
+document.getElementById('karna').style.backgroundColor = "#6e6363";
+
+
+document.getElementById("hi").style.color = "black";
+
+  document.getElementById("c").style.color = "black"
+}
+
+
+
+function green(){
+  document.body.style.backgroundColor = "green";
+
+    document.getElementById('karna').style.backgroundColor = "#86ed80";
+
+
+    document.getElementById("hi").style.color = "#ff4400";
+
+  document.getElementById("c").style.color = "#ff4400"
+}
+
+function red(){
+  document.body.style.backgroundColor = "red";
+
+    document.getElementById('karna').style.backgroundColor = "#ed8080";
+
+    document.getElementById("hi").style.color = "white";
+
+  document.getElementById("c").style.color = "white"
+}
+
+function yellow(){
+  document.body.style.backgroundColor = "yellow";
+  
+    document.getElementById('karna').style.backgroundColor = "#faf898";
+
+    document.getElementById("hi").style.color = "green";
+
+  document.getElementById("c").style.color = "green"
+}
+
